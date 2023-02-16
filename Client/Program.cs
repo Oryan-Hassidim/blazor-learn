d38eeae5fc5ede68d613fbade62f91222415aa25
Oryan Hassidim <oryan.hassidim@gmail.com>
2023-02-16 22:38:47 +0200

    auth

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient
+    {
+        BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) 
+    })
+    .AddLogging();
 
 await builder.Build().RunAsync();
